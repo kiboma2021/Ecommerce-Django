4c6dcf4a8d705a87af0e0d965895aa3e518bf6b4
kiboma <omayioben@gmail.com>
2023-01-11 19:30:58 +0300

    Update views

--- products/views.py
+++ products/views.py
@@ -3,4 +3,5 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse("I love coding and programming")
+    context={}
+    return render("index.html",context)
